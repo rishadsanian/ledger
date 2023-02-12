5465977e58df64721c6742229e86e5b98599ab29
Rishad Alam <rishad.s.a@gmail.com>
2023-02-12 00:44:13 -0500

    basic structure and initial commit

---
+++ ledgerjs.js
@@ -0,0 +1,121 @@
+/*Welcome to the ledger journey.
+
+This code will attempt to output of an accounting system application software */
+
+//Core//
+const software = { Ledger }
+const version = [0.01]
+const releasedate = ""
+let mission = []
+
+ 
+
+/*Ledger Architecture and customer journey
+user information collected/authenticated- and user profile role and group profile created
+
+
+
+LandPage
+  userlogin+Profle
+  homescreen : includes greeting, menu, quickbuttons
+    menu  
+      Journal Entry
+        Title Description Instruction
+        Entryform
+        Add Another
+        Entry Templates
+        Post
+          checkvalidation
+          post alert
+          homescreen
+        Cancel 
+          Cancel Alert
+          Back to homescreen 
+      General Journal
+      Accounts 
+      Settings
+        User
+        Accounts
+        Templates
+      Reporting
+        Balance Sheet
+        Income Statement
+        Cashflow Statement
+        Dashboard with Keymetrics
+      About Us
+
+  Logout
+Landing Page
+
+
+recording -checking -reporting* 
+Financial   Managerial Cost Accounting*/
+
+
+
+
+//User 
+
+userDatabase = [userId, userName, userEmail, userPassword, groupName, groupID, userRole,]
+userProfile = {}
+const userId = "" //generated/displayed
+const userName = [firstName, lastName]//Userinput//stored//concatenatefor
+const userEmail = ""
+const currency=""
+  //////Entry 
+
+
+  entry = {}
+const entryNumber = ""  //Generated/Displayed/Stored
+const dateTime = "" //Generated
+const accNumber = [] //userselect
+const accCategories = [] //userselect
+const amount = Number("").toFixed(2);
+const entryType = { debit: 1, credit: -1 }//
+const note = ""
+const noteCategory = []
+userId
+
+const vartypecheck = ()=> //checks variable types are correct
+const ledgercheck = () => { }//sum of debits and credits match to the same sum
+const balancesheetcheck = (datetimestamp) => { } //[Assets] = [Liabilities] + Equity]
+
+
+[debits]
+
+///Entry FrontEnd
+
+//Reporting 
+generalJournal = {}
+balanceSheet={}
+incomeStatement={}
+
+const accountingcycle=()=>//range, default is calendar year
+
+
+
+  //Quality Checks Functions
+
+  //Front End
+
+  =
+
+  BackEnd
+////Accounts
+
+////Variables 
+
+///Objects
+
+//Functions 
+
+const majorAmount = []
+const object_amount = {}
+
+//loops
+cont
+
+//Front End
+///Interface 
+
+
