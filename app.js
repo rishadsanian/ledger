3a7b2bc58367051e2a1241b61b2cc93faf348606
Rishad Alam <rishad.s.a@gmail.com>
2023-04-17 18:31:18 -0400

    new format

---
+++ app.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+  // Load journal entries from localStorage on page load
+  // deleteJournalEntries() //Clears Journal Entries first
+  loadJournalEntries();
+
+  // Submit form event handler
+  $("#journalForm").submit(function (event) {
+    event.preventDefault();
+    var account = $("#account").val();
+    var amountDR = $("#amountDR").val();
+    var amountCR = $("#amountCR").val();
+    var note = $("#note").val();
+
+    if (account && amountDR) {
+      addJournalEntry(account, amountDR, amountCR, note);
+      $("#account").val("");
+      $("#amountDR").val("");
+      $("#amountCR").val("");
+      $("#note").val("");
+      
+    } else if (account && amountCR) {
+      addJournalEntry(account, amountCR, amountDR);
+      $("#account").val("");
+      $("#amountDR").val("");
+      $("#amountCR").val("");
+      $("#note").val("");
+   
+    }
+  });
+});
+
+// Function to add a new journal entry to the table
+function addJournalEntry(account, amountDR, amountCR) {
+  var timestamp = new Date().toLocaleString();
+  var transactionNumber = generateTransactionNumber();
+  var userName = "User1";
+  var row =
+    "<tr><td>" +
+    userName +
+    "</td><td>" +
+    timestamp +
+    "</td><td>" +
+    transactionNumber +
+    "</td><td>" +
+    account +
+    "</td><td>" +
+    amountDR +
+    "</td><td>" +
+    amountCR +
+    "</td><td>" +
+    note +
+    "</td><td></td></tr>";
+  $("#journalEntriesTableBody").append(row);
+  saveJournalEntries();
+}
+
+// Function to load journal entries from localStorage
+function loadJournalEntries() {
+  var journalEntries = JSON.parse(localStorage.getItem("journalEntries"));
+  if (journalEntries) {
+    $("#journalEntriesTableBody").html(journalEntries);
+  }
+}
+
+// Function to save journal entries to localStorage
+function saveJournalEntries() {
+  var journalEntries = $("#journalEntriesTableBody").html();
+  localStorage.setItem("journalEntries", JSON.stringify(journalEntries));
+}
+
+// Function to generate a random transaction number
+function generateTransactionNumber() {
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var transactionNumber = "";
+  for (var i = 0; i < 8; i++) {
+    transactionNumber += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return transactionNumber;
+}
+// Function to delete journal entries from localStorage
+function deleteJournalEntries() {
+  localStorage.removeItem("journalEntries");
+}
