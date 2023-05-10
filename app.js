8acc6379101e0956fbc50cdc8728f89b4b38c507
Rishad Alam <rishad.s.a@gmail.com>
2023-05-10 18:51:13 -0400

    prettier format

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-
   //deleteJournalEntries() //Clears Journal Entries first
 
   // Load journal entries from localStorage on page load
   loadJournalEntries();
-
 
   //create form jquery function
 
@@ -12,54 +10,107 @@ $(document).ready(function () {
   var form = $("<form>", { id: "journalForm" });
   var entryNumber = 0;
   var lengthEntry = 2;
-  var entries = $("<div>").addClass('entries').css({ "display": "inline-block" });
+  var entries = $("<div>").addClass("entries").css({ display: "inline-block" });
 
   for (entryNumber = 0; entryNumber < lengthEntry; entryNumber++) {
     // Create the Account input field
-    var accountLabel = $("<label>", { for: "account" + (entryNumber + 1), html: "&nbsp Account: &nbsp" });
-    var accountInput = $("<input>", { type: "text", id: "account" + (entryNumber + 1) });
+    var accountLabel = $("<label>", {
+      for: "account" + (entryNumber + 1),
+      html: "&nbsp Account: &nbsp",
+    });
+    var accountInput = $("<input>", {
+      type: "text",
+      id: "account" + (entryNumber + 1),
+    });
 
     // Create the Debit Amount input field
-    var amountDRLabel = $("<label>", { for: "amountDR" + (entryNumber + 1), html: "&nbsp Debit Amount: &nbsp" });
-    var amountDRInput = $("<input>", { type: "number", id: "amountDR" + (entryNumber + 1) });
+    var amountDRLabel = $("<label>", {
+      for: "amountDR" + (entryNumber + 1),
+      html: "&nbsp Debit Amount: &nbsp",
+    });
+    var amountDRInput = $("<input>", {
+      type: "number",
+      id: "amountDR" + (entryNumber + 1),
+    });
 
     // Create the Credit Amount input field
-    var amountCRLabel = $("<label>", { for: "amountCR" + (entryNumber + 1), html: "&nbsp Credit Amount: &nbsp" });
-    var amountCRInput = $("<input>", { type: "number", id: "amountCR" + (entryNumber + 1) });
+    var amountCRLabel = $("<label>", {
+      for: "amountCR" + (entryNumber + 1),
+      html: "&nbsp Credit Amount: &nbsp",
+    });
+    var amountCRInput = $("<input>", {
+      type: "number",
+      id: "amountCR" + (entryNumber + 1),
+    });
 
     // Append input fields and labels to each entry
-    var entry = $("<div>").addClass('entry');
-    entry.append(accountLabel, accountInput, amountDRLabel, amountDRInput, amountCRLabel, amountCRInput);
+    var entry = $("<div>").addClass("entry");
+    entry.append(
+      accountLabel,
+      accountInput,
+      amountDRLabel,
+      amountDRInput,
+      amountCRLabel,
+      amountCRInput
+    );
 
     // Append each entry to entries div
     entries.append(entry);
   }
 
   // Create the Note input field
-  var notes = $("<div>").addClass('notes').css({ "display": "block" });
+  var notes = $("<div>").addClass("notes").css({ display: "block" });
   var noteLabel = $("<label>", { for: "note", html: "Note: &nbsp" });
   var noteInput = $("<input>", { type: "text", id: "note" });
   notes.append(noteLabel, noteInput);
 
   //Create Buttons
-  var buttons = $("<div>").addClass('buttons').css({ "display": "block" });
+  var buttons = $("<div>").addClass("buttons").css({ display: "block" });
 
   //Add row  button
 
   // Create a button to add another instance of input fields
-  var addButton = $("<button>").addClass("addRow").text("Add Row").click(function (event) {
-    event.preventDefault();
-    var newEntry = $("<div>").addClass('entry');
-    var newAccountLabel = $("<label>", { for: "account" + (lengthEntry + 1), html: "&nbsp Account: &nbsp" });
-    var newAccountInput = $("<input>", { type: "text", id: "account" + (lengthEntry + 1) });
-    var newAmountDRLabel = $("<label>", { for: "amountDR" + (lengthEntry + 1), html: "&nbsp Debit Amount: &nbsp" });
-    var newAmountDRInput = $("<input>", { type: "number", id: "amountDR" + (lengthEntry + 1) });
-    var newAmountCRLabel = $("<label>", { for: "amountCR" + (lengthEntry + 1), html: "&nbsp Credit Amount: &nbsp" });
-    var newAmountCRInput = $("<input>", { type: "number", id: "amountCR" + (lengthEntry + 1) });
-    newEntry.append(newAccountLabel, newAccountInput, newAmountDRLabel, newAmountDRInput, newAmountCRLabel, newAmountCRInput);
-    entries.append(newEntry);
-    lengthEntry++;
-  });
+  var addButton = $("<button>")
+    .addClass("addRow")
+    .text("Add Row")
+    .click(function (event) {
+      event.preventDefault();
+      var newEntry = $("<div>").addClass("entry");
+      var newAccountLabel = $("<label>", {
+        for: "account" + (lengthEntry + 1),
+        html: "&nbsp Account: &nbsp",
+      });
+      var newAccountInput = $("<input>", {
+        type: "text",
+        id: "account" + (lengthEntry + 1),
+      });
+      var newAmountDRLabel = $("<label>", {
+        for: "amountDR" + (lengthEntry + 1),
+        html: "&nbsp Debit Amount: &nbsp",
+      });
+      var newAmountDRInput = $("<input>", {
+        type: "number",
+        id: "amountDR" + (lengthEntry + 1),
+      });
+      var newAmountCRLabel = $("<label>", {
+        for: "amountCR" + (lengthEntry + 1),
+        html: "&nbsp Credit Amount: &nbsp",
+      });
+      var newAmountCRInput = $("<input>", {
+        type: "number",
+        id: "amountCR" + (lengthEntry + 1),
+      });
+      newEntry.append(
+        newAccountLabel,
+        newAccountInput,
+        newAmountDRLabel,
+        newAmountDRInput,
+        newAmountCRLabel,
+        newAmountCRInput
+      );
+      entries.append(newEntry);
+      lengthEntry++;
+    });
 
   // Create the Submit input button
   var submitInput = $("<input>", { type: "submit", value: "Submit" });
@@ -91,7 +142,6 @@ $(document).ready(function () {
         addJournalEntry(account, amountDR, amountCR, note);
         $("#account" + (i + 1)).val("");
         $("#amountDR" + (i + 1)).val("");
-
       } else if (account && amountCR) {
         addJournalEntry(account, amountDR, amountCR, note);
         $("#account" + (i + 1)).val("");
@@ -99,7 +149,7 @@ $(document).ready(function () {
       }
     }
     $("#note").val("");
-    location.reload(true)
+    location.reload(true);
   });
 });
 
@@ -145,10 +195,13 @@ function saveJournalEntries() {
 
 // Function to generate a random transaction number
 function generateTransactionNumber() {
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var transactionNumber = "";
   for (var i = 0; i < 8; i++) {
-    transactionNumber += characters.charAt(Math.floor(Math.random() * characters.length));
+    transactionNumber += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
   }
   return transactionNumber;
 }
@@ -236,4 +289,3 @@ function loadJournalEntries() {
   }
 }
 */
-
