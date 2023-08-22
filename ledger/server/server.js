c5b56edb31cc398e1bb9b109489728dcf92010cd
Rishad Alam <rishad.s.a@gmail.com>
2023-08-22 12:22:31 -0400

    project setup - connect to psql, add morgan and nodemon to package

--- ledger/server/server.js
+++ ledger/server/server.js
@@ -1,30 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000; // Use a port of your choice
+const { Pool, Client } = require("pg");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+//--------------------------------------------------------------------------//
+dotenv.config();
+app.use(morgan("dev"));
 
+//-------------------------------------------------------------------------//
+
+const PORT = process.env.PORT || 8080; // Use a port of your choice
 
 //----------------------------------------------------------------------------//
-const { Pool, Client } = require('pg');
 
-// Create a new Pool instance (recommended for handling multiple connections)
+// Create a new Pool instance
 const pool = new Pool({
-  user: 'your_username',
-  host: 'your_host',
-  database: 'your_database',
-  password: 'your_password',
-  port: 5432 // Default PostgreSQL port
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 // Test the database connection
-pool.query('SELECT NOW()', (err, res) => {
+pool.query("SELECT NOW()", (err, res) => {
   if (err) {
-    console.error('Error connecting to database:', err);
+    console.error("Error connecting to database:", err);
   } else {
-    console.log('Connected to database at:', res.rows[0].now);
+    console.log("Connected to database at:", res.rows[0].now);
   }
+});
+
+//----------------------------------------------------------------------------//
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
