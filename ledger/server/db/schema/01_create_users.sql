ca5c0b4a5f807859fef6a844cd265266cba2baaf
Rishad Alam <rishad.s.a@gmail.com>
2023-09-05 10:31:37 -0400

    db - add company to users schema

--- ledger/server/db/schema/01_create_users.sql
+++ ledger/server/db/schema/01_create_users.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
+    company VARCHAR(255),
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
