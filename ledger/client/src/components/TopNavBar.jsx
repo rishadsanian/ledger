3fe8a8263560a2e4d8a6b89a3163fe9422585eeb
Rishad Alam <rishad.s.a@gmail.com>
2023-09-06 12:25:14 -0400

    fix broken image link

--- ledger/client/src/components/TopNavBar.jsx
+++ ledger/client/src/components/TopNavBar.jsx
@@ -1,16 +1,16 @@
 // TopNavbar.js
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React from "react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 const TopNavbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        <img
-      src="https://github.com/rishadsanian/ledger/blob/main/logos/logoBlue.png?raw=true "
-      alt="ledger"
-    />
+          <img
+            src="https://github.com/rishadsanian/ledger/assets/77033627/10290488-d4ea-4b3c-bd1d-f8b4e7d078ba  "
+            alt="ledger"
+          />
         </Typography>
         <Button color="inherit">Home</Button>
         <Button color="inherit">About</Button>
