59c3d603a94d73d43780df43726411f20e9a0938
Rishad Alam <rishad.s.a@gmail.com>
2024-01-10 09:11:59 -0500

    tweak forms

--- ledger/client/src/components/accounts/CreateAccountForm.jsx
+++ ledger/client/src/components/accounts/CreateAccountForm.jsx
@@ -1,25 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { TextField } from "@mui/material";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { MdAddCircle } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+
 import {
   Button,
   // Select,
   // MenuItem,
   // FormControl,
   // InputLabel,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  Typography,
 } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
 
 //Account Types
 const accountTypeOptions = ["Debit", "Credit"];
 
-// classes
+// classes -
 const classesData = [
-  { id: 1, name: "Assets" },
-  { id: 2, name: "Liabilities" },
-  { id: 3, name: "Revenue" },
-  { id: 4, name: "Expense" },
-  { id: 5, name: "Equity" },
+  { id: 10, name: "Assets" },
+  { id: 20, name: "Liabilities" },
+  { id: 30, name: "Revenue" },
+  { id: 40, name: "Expense" },
+  { id: 50, name: "Equity" },
 ];
 
 function CreateAccount() {
@@ -28,6 +37,11 @@ function CreateAccount() {
   // const [balance, setBalance] = useState("");
   const [accountType, setAccountType] = useState("");
   const [selectedClass, setSelectedClass] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    console.log("isLoading:", isLoading);
+  }, [isLoading]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -40,6 +54,9 @@ function CreateAccount() {
     };
 
     try {
+      // Set loading state to true
+      setIsLoading(true);
+
       // Send a POST request to create the account
       const response = await Axios.post("/api/accounts", newAccount);
 
@@ -52,71 +69,100 @@ function CreateAccount() {
       // setBalance("");
       setAccountType("");
       setSelectedClass(null);
+      setIsLoading(false);
     } catch (error) {
+      setIsLoading(false);
       // Handle errors, for example, show an error message
       console.error("Error creating account:", error);
     }
   };
 
   return (
-    <div className="flex flex-col gap-3">
-      <div className="text-2xl pt-2 pb-4 bg-gray-900 text-white w-full">Create New Account</div>
-
-      <div className="text-white ">
-        <i className="fas fa-times"></i>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div class="flex flex-col gap-4">
-        <TextField
-          label="Name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-          fullWidth
-          // class="w-full "
-        />
-        
-        <TextField
-          label="Account Number"
-          type="text"
-          value={accountNumber}
-          onChange={(e) => setAccountNumber(e.target.value)}
-          required
-          fullWidth
-        />
-
-        <Autocomplete
-          id="account-type-select"
-          options={accountTypeOptions}
-          value={accountType}
-          onChange={(event, newValue) => setAccountType(newValue)}
-          renderInput={(params) => (
-            <TextField {...params} label="Account Type" fullWidth />
-          )}
-        />
-
-        <Autocomplete
-          id="class-select"
-          options={classesData}
-          getOptionLabel={(option) => option.name}
-          value={selectedClass}
-          onChange={(event, newValue) => setSelectedClass(newValue)}
-          renderInput={(params) => (
-            <TextField {...params} label="Select Class" fullWidth />
-          )}
-        />
+    <div className="flex flex-col gap-3 w-full border-2 bg-white rounded justify-between ">
+      <div className="flex justify-between items-center bg-gray-300 px-3">
+        <div className="flex  text-md pt-4 pb-4 text-gray-500 w-full">
+          Create New Account
         </div>
-        <div className="mt-8">
-        <Button
-          type="submit"
-          variant="contained"
-          color="tertiary"
-          className="buttons formwrapper"
-          style={{ color: 'background' }}
+        <div className="text-gray-500 cursor-pointer p-1 ">
+          <MdClose className="text-xl hover: text-gray-500 hover:scale-125 transition-transform" />
+        </div>
+      </div>
+
+      <form onSubmit={handleSubmit} class="px-4 py-4 ">
+        <div class="flex flex-col gap-4">
+          <TextField
+            label="Name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            fullWidth
+            // class="w-full "
+          />
+
+          <TextField
+            label="Account Number"
+            type="text"
+            value={accountNumber}
+            onChange={(e) => setAccountNumber(e.target.value)}
+            required
+            fullWidth
+          />
+
+          <Autocomplete
+            id="account-type-select"
+            options={accountTypeOptions}
+            value={accountType}
+            onChange={(event, newValue) => setAccountType(newValue)}
+            renderInput={(params) => (
+              <TextField {...params} label="Account Type" fullWidth />
+            )}
+          />
+
+          <Autocomplete
+            id="class-select"
+            options={classesData}
+            getOptionLabel={(option) => option.name}
+            value={selectedClass}
+            onChange={(event, newValue) => setSelectedClass(newValue)}
+            renderInput={(params) => (
+              <TextField {...params} label="Select Class" fullWidth />
+            )}
+          />
+        </div>
+
+        <FormControl component="fieldset">
+          <Typography variant="subtitle1" gutterBottom>
+            Account Type
+          </Typography>
+          <RadioGroup
+            id="account-type-select"
+            aria-label="account-type"
+            name="account-type"
+            value={accountType}
+            style={{ flexDirection: "row" }}
+          >
+            {accountTypeOptions.map((option) => (
+              <FormControlLabel
+                key={option}
+                value={option}
+                control={<Radio />}
+                label={option}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+        <div
+          className={`flex justify-center items-center mt-6 ${
+            isLoading ? "animate-spin" : ""
+          }`}
         >
-          Create Account
-        </Button>
+          <div class="w-full flex justify-center items-center">
+            <MdAddCircle
+              className="text-5xl text-gray-400 cursor-pointer hover:scale-125 transition-transform"
+              onClick={handleSubmit}
+            />
+          </div>
         </div>
       </form>
     </div>
--- ledger/client/src/components/accounts/MainAccounts.jsx
+++ ledger/client/src/components/accounts/MainAccounts.jsx
@@ -7,8 +7,8 @@ const MainAccounts = () => {
   return (
     <div className="flex flex-col "> 
       <div className="text-5xl mb-4 py-2 px-3"> Accounts</div>
-      <div className="bg-gray-100 min-h-screen flex">
-        <div className="w-1/2 bg-white p-8">
+      <div className="bg-gray-100 min-h-screen flex bg-gray-100">
+        <div className="w-1/2  p-8">
           {/* <h2 className="text-2xl font-semibold mb-4">Create New Account</h2> */}
           <CreateAccountForm />
         </div>
