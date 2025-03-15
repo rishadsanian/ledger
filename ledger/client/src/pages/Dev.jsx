fdc245e13f75a32d81ae7bfccf0f32fb47b296df
Rishad Alam <rishad.s.a@gmail.com>
2025-03-14 22:05:22 -0400

    adding dev page for component building and theming

---
+++ ledger/client/src/pages/Dev.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logos/logo.png';
+import '../styles/Landing.css'; // Assuming you want to separate styles into a CSS file
+
+const Dev = () => {
+  return (
+    <div className="landing-container">
+     <h1>Dev</h1>
+    </div>
+  );
+};
+
+export default Dev;
