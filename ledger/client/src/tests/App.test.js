cce088fa6d9712c76dcb2de0d93bef6d55809a75
Rishad Alam <77033627+rishadsanian@users.noreply.github.com>
2025-06-21 11:35:17 -0400

    Update dashboard test

--- ledger/client/src/tests/App.test.js
+++ ledger/client/src/tests/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders dashboard with Total Income text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const metricElement = screen.getByText(/Total Income/i);
+  expect(metricElement).toBeInTheDocument();
 });
